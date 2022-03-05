652c5e019ca07f971af4335916382157610d3c75
ZiKang <90165886+CHONZIKANG@users.noreply.github.com>
2022-03-05 17:21:54 +0800

    Add files via upload

---
+++ User_Address.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head> 
+
+<link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="CSS/user_address.css">
+
+</head>
+
+<body style="background-color:#f4f4f4;">
+
+
+<div id="info">
+<div id="left">
+<p>Manage My Account</p>
+<br>
+<a href="User_Profile.html">My Profile</a>
+<br>
+<br>
+<a href="User_Addresses.html" id="sub">&emsp;Addresses</a>
+<br>
+<br>
+<a href="Change_Password.html" id="sub">&emsp;Change Password</a>
+<br>
+<br>
+<a href="User_Purchase.html">My Purchase</a>
+
+</div>
+
+<div id="right">
+<div id="title">
+<h3>Addresses</h3>
+</div>
+<br>
+<br>
+<table border="1" style="width:100%;">
+<tr>
+	<th>Full Name</th>
+	<th>Phone</th>
+	<th>Address</th>
+	<th>Action</th>
+</tr>
+
+<tr>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+
+</tr>
+
+
+<tr>
+	<td></td>
+	<td></td>
+	<td></td>
+	<td></td>
+</tr>
+
+
+</table>
+
+<br>
+<br>
+<a href="add_address.html" id="add_btn">+ Add New Address</a>
+
+</div>
+</body>
+</html>
