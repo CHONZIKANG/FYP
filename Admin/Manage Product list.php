5e53c90b36bad80b6626ad93b49ab0b4119292b4
SeetKokPeng <1201200333@student.mmu.edu.my>
2022-04-18 20:51:02 +0800

    Update Manage Product list.php

--- Admin/Manage Product list.php
+++ Admin/Manage Product list.php
@@ -66,7 +66,7 @@ body {
         <div class="row">
             <div class="col-12">
                 <nav class="navbar navbar-expand-xl navbar-light bg-light">
-                    <a class="navbar-brand" href="index.html">
+                    <a class="navbar-brand" href="dashboard.php">
                         <i class="fas fa-3x fa-tachometer-alt tm-site-icon"></i>
                          <h1 class="tm-site-title mb-0">Electronic Gadgets Online<br> Admin Dashboard</h1>
                     </a>
@@ -78,7 +78,7 @@ body {
                      <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav mx-auto">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="index.html">Dashboard
+                                    <a class="nav-link" href="dashboard.php">Dashboard
                                         <span class="sr-only">(current)</span>
                                     </a>
                                 </li>
